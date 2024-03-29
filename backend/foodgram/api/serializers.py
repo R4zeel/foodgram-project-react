8dc6d1057c4e69ea4e28ee4f83d5600a151038e6
Антон Арефин <r4zeel@yandex.ru>
2024-03-29 16:52:28 +0300

    added image and color fields to serializers

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,9 +1,35 @@
+import base64
+
 from rest_framework import serializers
+from django.core.files.base import ContentFile
+import webcolors
 
 from .models import Recipe
 
 
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+    
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет названия')
+        return data
+
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+        return super().to_internal_value(data)
+
+
 class RecipeSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
     class Meta:
         model = Recipe
-        fields = ('name', 'description')
+        fields = ('name', 'description', 'image')
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
 
 from .models import Recipe
-
+from .serializers import RecipeSerializer
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    queryet = Recipe.objects.all()
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
